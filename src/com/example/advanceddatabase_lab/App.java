e129bd5c43c250839bdc2272203743ac3143a653
Philben Pierre <82547190+phillycodes@users.noreply.github.com>
2023-03-08 16:06:51 -0500

    I rearranged code and put the body of my if statements concerning the choice in methods so that I can run queries in methods instead of in the main. I did this for readability.

--- src/com/example/advanceddatabase_lab/App.java
+++ src/com/example/advanceddatabase_lab/App.java
@@ -22,6 +22,56 @@ public class App {
         return conn;
     }
 
+    public static void displaySwimmersAtLevel(int level) {
+    /*
+    System.out.println("Names and phones of all swimmers currently in level (of id) 3:");
+    System.out.println("fname" + " | " + "lname" + " | " + "phone");
+//Please insert your SELECT statement for query the data (Names and phones of all swimmers currently in level (of id) 3) into "statement.executeQuery()"
+    ResultSet resultSet = statement.executeQuery("");
+    while (resultSet.next()) {
+        System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname") + " | " + resultSet.getString("phone"));
+    }
+    sc.close();
+
+     */
+    }
+    public static void displayMultipleCaretakers() {
+    /*
+    System.out.println("Name(s) of caretakers who are the primary (main) caretakers of at least two swimmers:");
+    System.out.println("fname" + " | " + "lname");
+//Please insert your SELECT statement for query the data (Name(s) of caretakers who are the primary (main) caretakers of at least two swimmers) into "statement.executeQuery()"
+    ResultSet resultSet = statement.executeQuery("");
+    while (resultSet.next()) {
+        System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
+    }
+    sc.close();
+    */
+    }
+
+    public static void displayVolunteeredCaretakers() {
+    /*
+    System.out.println("Names of all caretakers who have volunteered for the task 'Recording' but not the task 'Officiating':");
+    System.out.println("fname" + " | " + "lname");
+    ResultSet resultSet = statement.executeQuery("select distinct c.FName, c.LName\r\n" +
+            "from Caretaker c \r\n" +
+            "where c.CT_Id in\r\n" +
+            " (select distinct c.CT_Id\r\n" +
+            "  from V_Task v join Commitment c\r\n" +
+            " on (v.VT_Id = c.VT_Id)\r\n" +
+            "  where v.Name ='Recording')\r\n" +
+            "and c.CT_Id not in\r\n" +
+            " (select distinct c.CT_Id\r\n" +
+            "  from V_Task v join Commitment c\r\n" +
+            " on (v.VT_Id = c.VT_Id)\r\n" +
+            "  where v.Name ='Officiating')");
+    while (resultSet.next()) {
+        System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
+    }
+    sc.close();
+
+     */
+    }
+
     public static void main(String[] args) {
         try (Connection connection =
                      DriverManager.getConnection(url, user, password)) { //"1122" is the password, and you need to replace it with your password
@@ -47,43 +97,10 @@ public class App {
             if (choice == '1') {
                 level = sc.nextInt();
                 displaySwimmersAtLevel(level);
-                System.out.println("Names and phones of all swimmers currently in level (of id) 3:");
-                System.out.println("fname" + " | " + "lname" + " | " +
-                        "phone");
-//Please insert your SELECT statement for query the data (Names and phones of all swimmers currently in level (of id) 3) into "statement.executeQuery()"
-                ResultSet resultSet = statement.executeQuery("");
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname") + " | " + resultSet.getString("phone"));
-                }
-                sc.close();
             } else if (choice == '2') {
-                System.out.println("Name(s) of caretakers who are the primary (main) caretakers of at least two swimmers:");
-                System.out.println("fname" + " | " + "lname");
-//Please insert your SELECT statement for query the data (Name(s) of caretakers who are the primary (main) caretakers of at least two swimmers) into "statement.executeQuery()"
-                ResultSet resultSet = statement.executeQuery("");
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
-                }
-                sc.close();
+                displayMultipleCaretakers();
             } else if (choice == '3') {
-                System.out.println("Names of all caretakers who have volunteered for the task 'Recording' but not the task 'Officiating':");
-                System.out.println("fname" + " | " + "lname");
-                ResultSet resultSet = statement.executeQuery("select distinct c.FName, c.LName\r\n" +
-                        "from Caretaker c \r\n" +
-                                "where c.CT_Id in\r\n" +
-                                " (select distinct c.CT_Id\r\n" +
-                                "  from V_Task v join Commitment c\r\n" +
-                                " on (v.VT_Id = c.VT_Id)\r\n" +
-                                "  where v.Name ='Recording')\r\n" +
-                                "and c.CT_Id not in\r\n" +
-                                " (select distinct c.CT_Id\r\n" +
-                                "  from V_Task v join Commitment c\r\n" +
-                                " on (v.VT_Id = c.VT_Id)\r\n" +
-                                "  where v.Name ='Officiating')");
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
-                }
-                sc.close();
+               displayVolunteeredCaretakers();
             } else if (choice == '4') {
                 sc.close();
                 System.exit(0);
@@ -95,8 +112,7 @@ public class App {
             System.out.println(e.getMessage());
         }
 }
-}
-
-public static void displaySwimmersAtLevel(int level) {
 
 }
+
+
