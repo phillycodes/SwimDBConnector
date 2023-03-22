590a385c9f1809128570343256a7160456abe7fd
Philben Pierre <82547190+phillycodes@users.noreply.github.com>
2023-03-22 11:44:31 -0400

    All methods connect and query specified data from the swimmingorg database. Comments added.

--- src/com/example/advanceddatabase_lab/App.java
+++ src/com/example/advanceddatabase_lab/App.java
@@ -4,13 +4,17 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class App {
-
+// login variables
     private final static String url = "jdbc:postgresql://localhost/postgres";
     private final static String user = "postgres";
     private final static String password = "Postgres21";
 
     public static Connection conn = null;
 
+    /*
+    * connect() creates a connection to the database using the provided credentials.
+    * The connection attempt must be surrounded by a try/catch statement in case the connection fails.
+     */
     public static Connection connect() {
         try {
             conn = DriverManager.getConnection(url, user, password);
@@ -20,8 +24,12 @@ public class App {
         return conn;
     }
 
+    /*
+    * displaySwimmerAtLevel(level) queries the swimmingorg database to find the swimmer(s) who are at a certain level
+    * Output columns: fname, lname, phone
+     */
     public static void displaySwimmersAtLevel(String level) {
-        System.out.println("First Name" + " | " + "Last Name" + " | " + "Phone");
+        System.out.println("fname" + " | " + "lname" + " | " + "phone");
         try (Connection conn = connect();
              Statement statement = conn.createStatement();
              ResultSet resultSet = statement.executeQuery("SELECT fname, lname, phone FROM swimmer WHERE currentlevelid = " + level)) {
@@ -33,51 +41,65 @@ public class App {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.out.println(e.getMessage());
         }
-//Please insert your SELECT statement for query the data (Names and phones of all swimmers currently in level (of id) 3) into "statement.executeQuery()"
 
 
     }
 
-    public static void displayMultipleCaretakers() {
     /*
-    System.out.println("Name(s) of caretakers who are the primary (main) caretakers of at least two swimmers:");
-    System.out.println("fname" + " | " + "lname");
-//Please insert your SELECT statement for query the data (Name(s) of caretakers who are the primary (main) caretakers of at least two swimmers) into "statement.executeQuery()"
-    ResultSet resultSet = statement.executeQuery("");
-    while (resultSet.next()) {
-        System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
-    }
-    sc.close();
-    */
-    }
-
-    public static void displayVolunteeredCaretakers() {
-    /*
-    System.out.println("Names of all caretakers who have volunteered for the task 'Recording' but not the task 'Officiating':");
-    System.out.println("fname" + " | " + "lname");
-    ResultSet resultSet = statement.executeQuery("select distinct c.FName, c.LName\r\n" +
-            "from Caretaker c \r\n" +
-            "where c.CT_Id in\r\n" +
-            " (select distinct c.CT_Id\r\n" +
-            "  from V_Task v join Commitment c\r\n" +
-            " on (v.VT_Id = c.VT_Id)\r\n" +
-            "  where v.Name ='Recording')\r\n" +
-            "and c.CT_Id not in\r\n" +
-            " (select distinct c.CT_Id\r\n" +
-            "  from V_Task v join Commitment c\r\n" +
-            " on (v.VT_Id = c.VT_Id)\r\n" +
-            "  where v.Name ='Officiating')");
-    while (resultSet.next()) {
-        System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
-    }
-    sc.close();
-
+    * displayMultipleCaretakers() queries the database swimmingorg to return the names of the swimmer(s) who has multiple
+    * caretakers.
+    * Output columns: fname, lname
      */
+    public static void displayMultipleCaretakers() {
+        System.out.println("fname" + " | " + "lname");
+
+        try (Connection conn = connect();
+             Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT DISTINCT c.fname, c.lname\r\n FROM caretaker c, swimmer s1, swimmer s2\r\n" +
+                     "where c.ct_id = s1.main_ct_id\r\n AND c.ct_id = s2.main_ct_id\r\n AND s1.swimmerid <> s2.swimmerid")) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("fname") + " | " +
+                        resultSet.getString("lname") + "\t| ");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /*
+    * displayVolunteeredCaretakers() will query the database swimmingorg to find the name of the caretakers who have
+    * volunteered for the task 'Recording' but not the task 'Officiating'.
+    * Output columns: fname, lname
+     */
+    public static void displayVolunteeredCaretakers() {
+        System.out.println("Names of all caretakers who have volunteered for the task 'Recording' but not the task 'Officiating':");
+        System.out.println("fname" + " | " + "lname");
+        try (Connection conn = connect();
+             Statement statement = conn.createStatement();
+             ResultSet resultSet = statement.executeQuery("select distinct c.FName, c.LName\r\n" +
+                     "from Caretaker c \r\n" +
+                     "where c.CT_Id in\r\n" +
+                     " (select distinct c.CT_Id\r\n" +
+                     "  from V_Task v join Commitment c\r\n" +
+                     " on (v.VT_Id = c.VT_Id)\r\n" +
+                     "  where v.Name ='Recording')\r\n" +
+                     "and c.CT_Id not in\r\n" +
+                     " (select distinct c.CT_Id\r\n" +
+                     "  from V_Task v join Commitment c\r\n" +
+                     " on (v.VT_Id = c.VT_Id)\r\n" +
+                     "  where v.Name ='Officiating')")) {
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("fname") + " | " + resultSet.getString("lname"));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void main(String[] args) {
-        try (Connection connection =
-                     DriverManager.getConnection(url, user, password)) {
+        try (Connection conn = connect()) {
 
             System.out.println("Java JDBC PostgreSQL swimmingorg");
             System.out.println("Connected to PostgreSQL database!");
@@ -97,7 +119,7 @@ public class App {
                 choice = sc.next().charAt(0);
 
                 switch (choice) {
-                    case '1':
+                    case '1' -> {
                         System.out.print("Enter Level: ");
                         level = scanner.nextInt();
                         System.out.println("");
@@ -105,45 +127,27 @@ public class App {
                         String levelString = levelInt.toString();
                         displaySwimmersAtLevel(levelString);
                         System.out.println();
-                        break;
-
-                    case '2':
-                        displayMultipleCaretakers();
-                        break;
-
-                    case '3':
-                        displayVolunteeredCaretakers();
-                        break;
-                    case '4':
+                    }
+                    case '2' -> displayMultipleCaretakers();
+                    case '3' -> displayVolunteeredCaretakers();
+                    case '4' -> {
                         sc.close();
                         scanner.close();
                         System.out.println("Goodbye!");
                         System.exit(0);
-                        break;
-                    default:
+                    }
+                    default -> {
                         System.out.println("You've entered an invalid character!");
                         sc.close();
                         scanner.close();
+                    }
                 }
-
-//            if (choice == '1') {
-//                level = sc.nextInt();
-//                displaySwimmersAtLevel(level);
-//            } else if (choice == '2') {
-//                displayMultipleCaretakers();
-//            } else if (choice == '3') {
-//               displayVolunteeredCaretakers();
-//            } else if (choice == '4') {
-//                sc.close();
-//                System.exit(0);
-//            }
             }
         } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.out.println(e.getMessage());
         }
     }
-
 }
 
 
